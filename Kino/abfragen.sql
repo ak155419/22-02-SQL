69accdc354bf4324b9b7f8433d4f5eed8759b0b3
ak155419 <123622266+ak155419@users.noreply.github.com>
2024-03-21 07:02:35 +0000

    hi

--- Kino/abfragen.sql
+++ Kino/abfragen.sql
@@ -1,1 +1,10 @@
 SELECT * FROM Film;
+--Challenge 1--
+SELECT  DISTINCT Jahr FROM Film
+ORDER BY Jahr
+SELECT MIN (Jahr) AS ältestes_Erscheinungsjahr FROM Film;
+ 
+--Challenge 2--
+SELECT DISTINCT Länge FROM Film
+ORDER BY Länge DESC
+LIMIT 1;

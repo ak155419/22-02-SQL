dfa5d4992d4c00c9c0a91b66d907e48d8a6abf24
ak155419 <123622266+ak155419@users.noreply.github.com>
2024-04-18 06:56:56 +0000

    Wi

--- Bestellung/create.sql
+++ Bestellung/create.sql
@@ -1,23 +1,26 @@
 CREATE TABLE Customers (
-    OrderID INT PRIMARY KEY,
-    CustomerID INT,
-    OrderDate DATE,
-    FOREIGN KEY (CustomerID) REFERENCES Orders(CustomerID)
+    CustomerID INTEGER PRIMARY KEY,
+    CustomerName TEXT NOT NULL,
+    ContactName TEXT NOT NULL,
+    Country TEXT NOT NULL
 );
- 
-INSERT INTO Customers (OrderID, CustomerID, OrderDate) VALUES
-(10308, 2, '1996-09-18'),
-(10309, 37, '1996-09-19'),
-(10310, 77, '1996-09-20');
 
 CREATE TABLE Orders (
-    CustomerID INT PRIMARY KEY,
-    CustomerName TEXT,
-    ContactName TEXT,
-    Country TEXT
+    OrderID INTEGER PRIMARY KEY,
+    CustomerID INTEGER NOT NULL,
+    OrderDate TEXT NOT NULL,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
- 
-INSERT INTO Orders (CustomerID, CustomerName, ContactName, Country) VALUES
-(2, 'Ana', 'Trujillo', 'Mexiko'),
-(1, 'Afreds Futterkiste', 'Maria Anders', 'Germany'),
-(3, 'Antoni Moreno Taqueria', 'Antoni Moreno', 'Mexiko');
+
+INSERT INTO Customers (CustomerID, CustomerName, ContactName, Country) VALUES
+(1, 'Alfreds Futterkiste', 'Maria Anders', 'Germany'),
+(2, 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Mexico'),
+(3, 'Antonio Moreno Taquería', 'Antonio Moreno', 'Mexico');
+
+INSERT INTO Orders (OrderID, CustomerID, OrderDate) VALUES
+(10308, 2, '1996-09-18'),
+(10365, 3, '1996-11-27'),
+(10383, 3, '1996-12-16'),
+(10355, 3, '1996-11-15'),
+(10278, 1, '1996-08-12');
+
